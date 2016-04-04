473f0b5ccdf9e2b705eae36e6ed07588a1ae0872
Pranav Ravichandran <me@onloop.net>
2016-04-04 01:26:29 -0400

    Updated content

--- res/content.js
+++ res/content.js
@@ -6,6 +6,70 @@ define([], function () {
                 'name': 'Becoming a Gator!',
                 'description': "This course is for international students joining the University of Florida this upcoming semester. UF hosts more than 6,000 international students who are seeking an education that is fully accredited and will be recognized all over the globe. As the state of Florida's largest and oldest university, the University of Florida is one the state's centers for education, medicine, cultural events and athletics. The university is located in Gainesville in North Central Florida, continually ranked as one of the best places to live in the United States. Subscribe to this course to get news, tips, and challenges to help you fit right in Gator-town!",
                 'rating': 4.8,
+                'completion': 40,
+                'reviews': [
+                    {
+                        'name': 'Dhruv',
+                        'content': 'This course really helped me settle into life at Gainesville. I loved the tiered challenge structure which I felt was nicely balanced in terms of slowly scaling up the difficulty and making things more interesting!'
+                    },
+                    {
+                        'name': 'Pooja',
+                        'content': 'This course has some really great tips about life in the Swamp. I honestly wouldn’t have come to known of a lot of great things UF has to offer if not for this course! 5/5, a definite subscribe.'
+                    },
+                    {
+                        'name': 'Zhang',
+                        'content': 'I would definitely recommend this course to any international student joining UF. The challenges are fun and you get to meet a lot of great people along the way!'
+                    }
+                ]
+            },
+            {
+                'id': 1,
+                'name': 'Welcome to the Sunshine State!',
+                'description': "Welcome to Florida, otherwise known as the sunshine state. Florida is a beautiful places with awesome beaches and natural beauty. You can basically fall out of bed and go tanning, swimming, surfing, fishing, boating, snorkeling and scuba diving to look at marine life, reefs and hidden treasure. There’s more to Florida than beaches and hidden springs, however. From historic and quaint St. Augustine to the hip and happening South Beach, there’s a place in Florida for everyone. Indeed, In Florida you’ll find people from just about everywhere: The Northeast, Midwest and from other countries as well. This melting pot of cultures literally is what the term suggests – a delectable blend of multicultural cuisines. Subscribe to our course to understand what it means to be a Floridian!",
+                'rating': 4.0,
+                'completion': 90,
+                'reviews': [
+                    {
+                        'name': 'Dhruv',
+                        'content': 'This course really helped me settle into life at Gainesville. I loved the tiered challenge structure which I felt was nicely balanced in terms of slowly scaling up the difficulty and making things more interesting!'
+                    },
+                    {
+                        'name': 'Pooja',
+                        'content': 'This course has some really great tips about life in the Swamp. I honestly wouldn’t have come to known of a lot of great things UF has to offer if not for this course! 5/5, a definite subscribe.'
+                    },
+                    {
+                        'name': 'Zhang',
+                        'content': 'I would definitely recommend this course to any international student joining UF. The challenges are fun and you get to meet a lot of great people along the way!'
+                    }
+                ]
+            },
+            {
+                'id': 2,
+                'name': 'UF Adventures',
+                'description': "The objective of this course is to provide the UF community with an opportunity for people of all skill levels to enjoy outdoor activities. Emphasis is placed on members learning to be competent at their activities of interest and not merely being led. There is no requirement of experience, dues, or special equipment. Subscribing to this course will put you in touch with a community of outdoorsy people, organize challenges for different skill levels, runs trips at cost, and provides necessary equipment.",
+                'rating': 4.7,
+                'completion': 10,
+                'reviews': [
+                    {
+                        'name': 'Dhruv',
+                        'content': 'This course really helped me settle into life at Gainesville. I loved the tiered challenge structure which I felt was nicely balanced in terms of slowly scaling up the difficulty and making things more interesting!'
+                    },
+                    {
+                        'name': 'Pooja',
+                        'content': 'This course has some really great tips about life in the Swamp. I honestly wouldn’t have come to known of a lot of great things UF has to offer if not for this course! 5/5, a definite subscribe.'
+                    },
+                    {
+                        'name': 'Zhang',
+                        'content': 'I would definitely recommend this course to any international student joining UF. The challenges are fun and you get to meet a lot of great people along the way!'
+                    }
+                ]
+            },
+            {
+                'id': 3,
+                'name': 'Graduate Student Handbook',
+                'description': "The UF Graduate School is committed to providing services to the campus community that maintain integrity and excellence in graduate education through clear and consistent policies, high standards, efficient procedures and direct student support. This course was launched as an effort to support all graduate students, faculty and staff by fostering relationships, increasing communications and collaborations, and delivering comprehensive research and data resources to inform graduate education. This course offers you a continuous stream of useful information and challenges that you can take on in order to make your study at University of Florida as fruitful as possible.",
+                'rating': 4.6,
+                'completion': 20,
                 'reviews': [
                     {
                         'name': 'Dhruv',
@@ -57,6 +121,105 @@ define([], function () {
                 'description': "Ben Hill Griffin Stadium at Florida Field, popularly known as The Swamp, is the football stadium for the University of Florida and the home field of the university's Florida Gators football team, and as a new gator, it is tradition to get down to the Swamp to take in the sights of one of the largest and historic stadiums in the country.",
                 'tier': 1,
                 'credits': 10,
+                'courseid': 0
+            },
+            {
+                'id': 4,
+                'name': 'Open a bank account',
+                'completed': true,
+                'description': "",
+                'tier': 2,
+                'credits': 20,
+                'courseid': 0
+            },
+            {
+                'id': 5,
+                'name': 'Attend the UF international Gator social',
+                'completed': true,
+                'description': "",
+                'tier': 2,
+                'credits': 20,
+                'courseid': 0
+            },
+            {
+                'id': 6,
+                'name': 'Have a fun night out at Gator nights!',
+                'completed': false,
+                'description': "",
+                'tier': 2,
+                'credits': 20,
+                'courseid': 0
+            },
+            {
+                'id': 7,
+                'name': 'Play bowling at the Reitz Union Game Room',
+                'completed': false,
+                'description': "",
+                'tier': 2,
+                'credits': 20,
+                'courseid': 0
+            },
+            {
+                'id': 8,
+                'name': 'Capture the sighting of a gator at Lake Alice.',
+                'completed': false,
+                'description': "",
+                'tier': 2,
+                'credits': 20,
+                'courseid': 0
+            },
+            {
+                'id': 9,
+                'name': 'Attend a football match at Ben Hill Griffin Stadium.',
+                'completed': false,
+                'description': "",
+                'tier': 3,
+                'credits': 30,
+                'courseid': 0
+            },
+            {
+                'id': 10,
+                'name': 'Visit UF RecSports.',
+                'completed': false,
+                'description': "",
+                'tier': 3,
+                'credits': 30,
+                'courseid': 0
+            },
+            {
+                'id': 11,
+                'name': 'Take a selfie with Albert or Alberta!',
+                'completed': false,
+                'description': "",
+                'tier': 3,
+                'credits': 30,
+                'courseid': 0
+            },
+            {
+                'id': 12,
+                'name': 'Try out 5 different cuisines you’ve never tried before.',
+                'completed': false,
+                'description': "",
+                'tier': 3,
+                'credits': 30,
+                'courseid': 0
+            },
+            {
+                'id': 13,
+                'name': 'Spend a day out at Lake Wauburg',
+                'completed': false,
+                'description': "",
+                'tier': 4,
+                'credits': 40,
+                'courseid': 0
+            },
+            {
+                'id': 14,
+                'name': 'Play a team sport with new people at UF RecSports.',
+                'completed': false,
+                'description': "",
+                'tier': 4,
+                'credits': 40,
                 'courseid': 0
             }
         ],
